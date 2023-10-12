38bf2286372f8a9644dca9a3739caf934e4abe22
arkanorun <arkanorun@gmail.com>
2023-10-11 21:32:27 -0300

    remoção do usuarioSchema no arquivo controladores/usuarios.js

--- src/controladores/usuarios.js
+++ src/controladores/usuarios.js
@@ -1,8 +1,9 @@
 const knex = require('../conexao')
 const bcrypt = require('bcrypt')
-const usuarioSchema = require('../validacoes/usuario')
+
 
 const cadastrarUsuario = async (req, res) => {
+
     const { nome, email, senha } = req.body
 
     try {
@@ -33,40 +34,41 @@ const cadastrarUsuario = async (req, res) => {
 }
 
 const perfilUsuario = (req, res) => {
+
     return res.json(req.usuario)
 }
 
-const editarUsuario = async (req,res) => {
+const editarUsuario = async (req, res) => {
 
-        const { nome, email, senha } = req.body;
+    const { nome, email, senha } = req.body;
 
-        const { id } = req.usuario;
+    const { id } = req.usuario;
 
-        try {
-            const usuarioBusca = await knex('usuarios').where({ id }).first()
-            if (usuarioBusca.email != email){
+    try {
+        const usuarioBusca = await knex('usuarios').where({ id }).first()
+        if (usuarioBusca.email != email) {
 
-                const usuarioBuscaPorEmail = await knex('usuarios').where({email}).first()
-                if (usuarioBuscaPorEmail){
-                    return res.status(400).json({ mensagem: 'o email informado já está cadastrado.'})
-                }
+            const usuarioBuscaPorEmail = await knex('usuarios').where({ email }).first()
+            if (usuarioBuscaPorEmail) {
+                return res.status(400).json({ mensagem: 'o email informado já está cadastrado.' })
             }
-            
-            const senhaCriptografada = await bcrypt.hash(senha, 10)  
-
-            const usuarioEditado = await knex('usuarios').update({
-                nome,
-                email,
-                senha: senhaCriptografada
-            }).where({id});
-
-            return res.status(200).json({mensagem: 'Usuário atualizado com sucesso.'});
-                 
-            
-
-        } catch (error) {    
-            
-            return res.status(500).json({ mensagem: error.message})
         }
+
+        const senhaCriptografada = await bcrypt.hash(senha, 10)
+
+        const usuarioEditado = await knex('usuarios').update({
+            nome,
+            email,
+            senha: senhaCriptografada
+        }).where({ id });
+
+        return res.status(200).json({ mensagem: 'Usuário atualizado com sucesso.' });
+
+
+
+    } catch (error) {
+
+        return res.status(500).json({ mensagem: error.message })
+    }
 }
 module.exports = { cadastrarUsuario, perfilUsuario, editarUsuario }
