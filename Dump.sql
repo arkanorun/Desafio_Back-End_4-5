2c1e82a1d18631d9c37dde79e4f23123a5bea2f7
arkanorun <arkanorun@gmail.com>
2023-10-16 22:06:12 -0300

    adição das tabelas produtos e clientes

--- Dump.sql
+++ Dump.sql
@@ -32,12 +32,20 @@ valor integer not null,
 categoria_id integer not null references categorias(id)
 );
 
+create table produtos(
+id serial primary key,
+descricao text not null,
+quantidade_estoque integer not null,
+valor integer not null,
+categoria_id integer not null references categorias(id)
+);
+
 create table clientes(
 id serial primary key,
 nome text not null,
 email text not null unique,
 cpf char(11) not null unique,
-cep varchar(20), 
+cep char(8), 
 rua text,
 numero integer,
 bairro text,
