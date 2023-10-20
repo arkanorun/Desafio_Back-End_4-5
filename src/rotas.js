ae14c4c0e35cc982dc65b3509f6ee957beb20080
Mattborges1 <mattborges1@gmail.com>
2023-10-20 16:56:57 -0400

    commit de segurança

--- src/rotas.js
+++ src/rotas.js
@@ -2,11 +2,12 @@ const { Router } = require('express')
 const { cadastrarUsuario, perfilUsuario, editarUsuario } = require('./controladores/usuarios')
 const rotas = Router()
 const validarCorpo = require('./intermediarios/validarCorpoRequisicao.js')
-const { loginSchema, usuarioSchema } = require('./validacoes/schema')
+const { loginSchema, usuarioSchema, clienteSchema } = require('./validacoes/schema')
 const { listarCategorias } = require('./controladores/categorias')
 const login = require('./controladores/autenticacao')
 
 const filtroLogin = require('./intermediarios/filtrarLogin')
+const { cadastrarCliente } = require('./controladores/clientes')
 
 
 
@@ -15,6 +16,8 @@ rotas.post('/usuario', validarCorpo(usuarioSchema), cadastrarUsuario)
 rotas.get('/categoria', listarCategorias)
 
 rotas.post('/login', validarCorpo(loginSchema), login)
+
+rotas.post('/cliente', validarCorpo(clienteSchema), cadastrarCliente)
 
 rotas.use(filtroLogin)
 
