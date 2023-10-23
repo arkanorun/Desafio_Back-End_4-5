3db4d844a4a818fdc8a03f8312df670f62765fe7
unknown <marianabarros.fb@gmail.com>
2023-10-23 14:23:12 -0400

    produto finalizado

--- src/controladores/produtos.js
+++ src/controladores/produtos.js
@@ -1,24 +1,29 @@
 const knex = require('../conexao')
 
 const listarProduto = async (req, res) => {
-
     const { categoria_id } = req.query;
-
-    let produtos;
 
     try {
         if (categoria_id) {
-            produtos = await knex('produtos').where('categoria_id', categoria_id);
+            const categoria = await knex('categorias').where('id', categoria_id).first();
+
+            if (categoria) {
+                const produtos = await knex('produtos').where('categoria_id', categoria_id);
+                if (produtos == 0) {
+                    return res.status(200).json({ mensagem: 'Nenhum produto foi cadastrado nessa categoria.' });
+                }
+                return res.status(200).json(produtos);
+            } else {
+                return res.status(404).json({ mensagem: 'Categoria não cadastrada.' });
+            }
         } else {
-            produtos = await knex('produtos').select('*');
+            const produtos = await knex('produtos').select('*');
+            return res.status(200).json(produtos);
         }
-
-        return res.status(200).json(produtos);
-
     } catch (error) {
         return res.status(500).json({ mensagem: 'Erro interno do servidor' });
     }
-
 }
+
 
 module.exports = { listarProduto }
