91e5c6dfcd1ecb4aa396a8c1852dcc37723ec824
Mattborges1 <mattborges1@gmail.com>
2023-11-07 18:19:28 -0500

    realizado o aprimoramento de exclusão e edição de produtos

--- src/aws.js
+++ src/aws.js
@@ -1,17 +1,17 @@
-const aws = require ('aws-sdk')
+const aws = require('aws-sdk')
 
 const endpoint = new aws.Endpoint(process.env.ENDPOINT_S3)
 
 const s3 = new aws.S3({
-        endpoint,
-        credentials: {
-            accessKeyId: process.env.KEY_ID,
-            secretAccessKey: process.env.APP_KEY
-        }
+    endpoint,
+    credentials: {
+        accessKeyId: process.env.KEY_ID,
+        secretAccessKey: process.env.APP_KEY
+    }
 })
 
 
-const uploadImagem = async (path, buffer, mimetype ) => {
+const uploadImagem = async (path, buffer, mimetype) => {
     const imagem = await s3.upload({
         Bucket: process.env.BUCKET,
         Key: path,
@@ -26,8 +26,16 @@ const uploadImagem = async (path, buffer, mimetype ) => {
 
 }
 
+const deletarImagem = async (path) => {
+    await s3.deleteObject({
+        Bucket: process.env.BUCKET,
+        Key: path
+    }).promise()
+}
+
 
 
 module.exports = {
-    uploadImagem
+    uploadImagem,
+    deletarImagem
 }
